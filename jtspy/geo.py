e59de1a696ce9e36d309dadda0883e5f0a244d36
Federico Botta <fb394@Q6M4CY5WKG.local>
2022-12-09 10:39:07 +0000

    Update geo endpoints to download directly from API

--- jtspy/geo.py
+++ jtspy/geo.py
@@ -1,5 +1,7 @@
 import geopandas as gpd
-
+from urllib.request import urlopen
+import json
+import pandas as pd
 
 def get_lsoa_boundaries():
     '''
@@ -19,7 +21,27 @@ def get_lsoa_boundaries():
     print('Hang on, this may take a little while...')
     print('\n\n\n')
     
-    lsoa_gdf = gpd.read_file('https://opendata.arcgis.com/datasets/1f23484eafea45f98485ef816e4fee2d_0.geojson')
+    url = 'https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/LSOA_Dec_2011_Boundaries_Super_Generalised_Clipped_BSC_EW_V3_2022/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&returnIdsOnly=true&outSR=4326&f=json'
+
+    response = urlopen(url)
+    json_data = response.read().decode()
+
+    id_list = json.loads(json_data)['objectIds']
+
+    lsoa_gdf = gpd.GeoDataFrame()
+
+    while id_list:
+        
+        temp_id = id_list[0:250]
+        
+        temp_url = 'https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/LSOA_Dec_2011_Boundaries_Super_Generalised_Clipped_BSC_EW_V3_2022/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json&objectIds=' + ','.join(str(x) for x in temp_id)
+        
+        temp_gdf = gpd.read_file(temp_url)
+        
+        lsoa_gdf = gpd.GeoDataFrame(pd.concat([lsoa_gdf, temp_gdf]))
+        
+        id_list = [x for x in id_list if x not in temp_id]
+    
     #lsoa_gdf.to_crs('EPSG:27700', inplace = True)
     lsoa_gdf.to_crs(' EPSG:3857', inplace = True)
     
@@ -43,8 +65,12 @@ def get_la_boundaries():
     print('Hang on, this may take a little while...')
     print('\n\n\n')
     
-    la_gdf = gpd.read_file('https://opendata.arcgis.com/datasets/0c09b7cde8b44c4ab6e2a1e47a91e400_0.geojson')
+    la_gdf = gpd.read_file('https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Local_Authority_Districts_December_2011_FCB_EW_2022/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson')
     #lsoa_gdf.to_crs('EPSG:27700', inplace = True)
     la_gdf.to_crs(' EPSG:3857', inplace = True)
     
     return la_gdf
+
+
+
+

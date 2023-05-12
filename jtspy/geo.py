6ed9966d90dab816c6d48b7e54cf6d386f5aa68d
Federico Botta <federico.botta@wbs.ac.uk>
2023-05-12 13:59:41 +0100

    Update geo.py

--- jtspy/geo.py
+++ jtspy/geo.py
@@ -21,7 +21,7 @@ def get_lsoa_boundaries():
     print('Hang on, this may take a little while...')
     print('\n\n\n')
     
-    url = 'https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/LSOA_Dec_2011_Boundaries_Super_Generalised_Clipped_BSC_EW_V3_2022/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&returnIdsOnly=true&outSR=4326&f=json'
+    url = 'https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/LSOA_Dec_2011_Boundaries_Generalised_Clipped_BGC_EW_V3_2022/FeatureServer/0/query?where=1%3D1&outFields=*&returnIdsOnly=true&outSR=4326&f=json'
 
     response = urlopen(url)
     json_data = response.read().decode()
@@ -34,15 +34,14 @@ def get_lsoa_boundaries():
         
         temp_id = id_list[0:250]
         
-        temp_url = 'https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/LSOA_Dec_2011_Boundaries_Super_Generalised_Clipped_BSC_EW_V3_2022/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json&objectIds=' + ','.join(str(x) for x in temp_id)
+        temp_url = 'https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/LSOA_Dec_2011_Boundaries_Generalised_Clipped_BGC_EW_V3_2022/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json&objectIds=' + ','.join(str(x) for x in temp_id)
         
         temp_gdf = gpd.read_file(temp_url)
         
         lsoa_gdf = gpd.GeoDataFrame(pd.concat([lsoa_gdf, temp_gdf]))
         
         id_list = [x for x in id_list if x not in temp_id]
-    
-    #lsoa_gdf.to_crs('EPSG:27700', inplace = True)
+        
     lsoa_gdf.to_crs(' EPSG:3857', inplace = True)
     
     return lsoa_gdf
